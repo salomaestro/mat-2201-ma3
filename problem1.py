6ef4dd9f1ce549f0449517ea1c8220aa70a6f068
salomaestro <chris10an.salomonsen@gmail.com>
2022-11-12 00:43:50 +0100

    polynomial

---
+++ problem1.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+sns.set_theme(style="darkgrid")
+
+x = np.linspace(0, 3, 100)
+
+# f = lambda x: 3/2 * x ** 2 + 0.5 * x + 1
+
+f = lambda x: 1 + x + 2 * (x-0) * (x-1)
+
+y = f(x)
+
+plt.plot(x, y)
+plt.scatter([0, 1, 2], [1, 2, 7], color="red")
+plt.show()
